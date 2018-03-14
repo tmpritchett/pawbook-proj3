c27b1e73a41c89e3e333a0cbd6312deab04fa143
Tanya M Pritchett <tanyampritchett@Tanyas-MacBook-Pro.local>
2018-03-14 18:13:34 -0400

    added to json

---
+++ server.js
@@ -0,0 +1,27 @@
+require ("dotenv").config();
+const express = require('express');
+const bodyParser = require ('body-parser');
+const mongoose = require ('mongoose');
+const morgan = require ('morgan');
+const app = express();
+mongoose.connect(process.env.MONGODB_URI);
+
+const connection = mongoose.connection;
+connection.on ('connected', () => {
+    console.log('Mongoose Connected Successfully')
+})
+
+connection.on('error', (err) => {
+    console.log('Mongoose default connection error: ' + err);
+
+})
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Magic happening on port " + PORT);
+
+})
