19c8018a2b9a4bd9d2fe67ad0b51f3f5b6aaf4f0
Tanya M Pritchett <tanyampritchett@Tanyas-MacBook-Pro.local>
2018-03-15 15:02:56 -0400

    added styling to user page

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import dalmation from '../images/dalmation.jpg'
+
+const HomePage = styled.div`
+margin: 0;
+padding: 7px;
+height: 10vh;
+background: black;
+overflow: hidden;
+
+`
+const Image = styled.img `
+margin-left: 3vw;
+height:100%;
+width: auto;
+&:hover{
+margin-top:5px;
+}
+`
+
+
+
+class LandingPage extends Component {
+
+    render() {
+        
+        return (
+            <HomePage>
+             <Link to="/"><Image src={dalmation} alt="dalmation"/></Link>
+            </HomePage>
+        )
+    }
+
+}
+
+export default LandingPage
--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import style from 'styled-components'
+import axios from 'axios'
+import LandingPage from './components/LandingPage'
 
 class UserPage extends Component {
     render() {
         return (
             <div>
-                Hello
+                <LandingPage />
             </div>
         );
     }
