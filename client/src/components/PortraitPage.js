4668194ac419d55cfb9e72cbe44fb34d55906732
Tanya M Pritchett <tanyampritchett@Tanyas-MacBook-Pro.local>
2018-03-15 13:01:37 -0400

    added to portrait page

---
+++ client/src/components/PortraitPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PortraitPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Portrait Page </h1>
+            </div>
+        );
+    }
+}
+
+export default PortraitPage;
+
